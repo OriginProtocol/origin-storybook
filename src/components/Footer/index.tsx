b130c5f7375dbb691137a6bc0781572266814a33
Nick Hong <nickhong@originprotocol.com>
2022-09-29 15:06:57 -0400

    Finish footer linking

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { Discord, Facebook, Medium, OriginLogo, Telegram, Twitter, YouTube } from "../Icons"
+import { Discord, Facebook, FTXIcon, Medium, OriginLogo, Reddit, RedditIcon, Telegram, Twitter, YouTube } from "../Icons"
 import { Typography } from "../Typography"
 
 export const Footer = () => (
@@ -6,13 +6,31 @@ export const Footer = () => (
     <div className="border-b flex flex-col md:flex-row space-y-12 md:space-y-0 pb-12 md:pb-7 px-9 max-w-screen-xl mx-auto" style={{borderBottomColor: 'rgba(255,255,25,0.2)'}}>
       <div className="flex flex-col md:justify-between space-y-6 md:space-y-0 flex-6">
         <OriginLogo webProperty="originprotocol" theme="dark" />
-        <div className="flex space-x-4 items-center">
-          <Twitter />
-          <Facebook />
-          <YouTube />
-          <Medium />
-          <Telegram />
-          <Discord />
+        <div className="flex space-x-6 items-center">
+          <a href='https://twitter.com/OriginProtocol' target='_blank'>
+            <Twitter />
+          </a>
+          <a href='https://www.facebook.com/originprotocol/' target='_blank'>
+            <Facebook />
+          </a>
+          <a href='https://www.youtube.com/c/originprotocol' target='_blank'>
+            <YouTube />
+          </a>
+          <a href='https://blog.originprotocol.com/' target='_blank'>
+            <Medium />
+          </a>
+          <a href='https://t.me/originprotocol' target='_blank'>
+            <Telegram />
+          </a>
+          <a href='https://discord.com/invite/ogn' target='_blank'>
+            <Discord />
+          </a>
+          <a href='https://www.facebook.com/originprotocol/' target='_blank'>
+            <RedditIcon />
+          </a>
+          <a href='https://blockfolio.com/coin/OGN' target='_blank'>
+            <FTXIcon />
+          </a>
         </div>
       </div>
       <div className="flex flex-col flex-2 space-y-4">
@@ -21,14 +39,20 @@ export const Footer = () => (
         </Typography.Body2>
         <div className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4">
           <Typography.Body2 className="opacity-75" href='https://google.com'>
-            Team
+            <a href='https://www.originprotocol.com/en/team' target='_blank'>
+              Team
+            </a>
           </Typography.Body2>
           <Typography.Body2 className="opacity-75" href='https://google.com'>
-            Products
+            <a href='https://www.originprotocol.com/en/dashboard' target='_blank'>
+              Products
+            </a>
           </Typography.Body2>
           <div className="flex items-center">
             <Typography.Body2 className="opacity-75" href='https://google.com'>
-              Careers
+              <a href='https://angel.co/company/originprotocol/jobs' target='_blank'>
+                Careers
+              </a>
             </Typography.Body2>
             <a href='https://google.com' target='_blank'>
               <div className="opacity-100 text-sm ml-2 bg-gray-700 py-1 px-3 rounded-xl font-medium">
@@ -44,10 +68,14 @@ export const Footer = () => (
         </Typography.Body2>
         <div className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4 opacity-75">
           <Typography.Body2>
-            Origin Story
+            <a href='https://story.xyz' target='_blank'>
+              Origin Story
+            </a>
           </Typography.Body2>
           <Typography.Body2>
-            Origin Dollar
+            <a href='https://ousd.com' target='_blank'>
+              Origin Dollar
+            </a>
           </Typography.Body2>
         </div>
       </div>
@@ -57,16 +85,24 @@ export const Footer = () => (
         </Typography.Body2>
         <div className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4 opacity-75">
           <Typography.Body2>
-            Litepaper
+            <a href='https://www.originprotocol.com/en/litepaper' target='_blank'>
+              Litepaper
+            </a>
           </Typography.Body2>
           <Typography.Body2>
-            Videos
+            <a href='https://www.originprotocol.com/en/videos' target='_blank'>
+              Videos
+            </a>
           </Typography.Body2>
           <Typography.Body2>
-            GitHub
+            <a href='https://github.com/originprotocol' target='_blank'>
+              GitHub
+            </a>
           </Typography.Body2>
           <Typography.Body2>
-            Governance
+            <a href='https://vote.originprotocol.com' target='_blank'>
+              Governance
+            </a>
           </Typography.Body2>
         </div>
       </div>
@@ -77,10 +113,14 @@ export const Footer = () => (
       </Typography.Body2>
       <div className="flex space-x-6">
         <Typography.Body2>
-          Privacy Policy
+          <a href='https://www.originprotocol.com/en/privacy' target='_blank'>
+            Privacy Policy
+          </a>
         </Typography.Body2>
         <Typography.Body2>
-          Terms of Service
+          <a href='https://www.originprotocol.com/en/tos' target='_blank'>
+            Terms of Service
+          </a>
         </Typography.Body2>
       </div>
     </div>
