354c67f6bf9d624937bf9403a431ef1bdd7d40cc
Nick Hong <me@nickhong.xyz>
2022-11-03 12:29:53 -0700

    Remove objectFit in card stories

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -42,7 +42,6 @@ OriginProtocolCard.args = {
       src="https://placekeanu.com/128/128"
       alt="keanu face"
       layout="fill"
-      objectFit="cover"
     />
   ),
   body: "7,777 Lucky Duckies have left the pond and are getting into mischief!",
@@ -69,7 +68,6 @@ OUSDCard.args = {
       src="https://placekeanu.com/128/128"
       alt="keanu face"
       layout="fill"
-      objectFit="cover"
     />
   ),
   body: "7,777 Lucky Duckies have left the pond and are getting into mischief!",
@@ -99,7 +97,6 @@ StoryCard.args = {
       src="https://placekeanu.com/128/128"
       alt="keanu face"
       layout="fill"
-      objectFit="cover"
     />
   ),
   body: "3LAU's sale on Origin generated $11.7M in sales and holds the world record for the most successful music NFT of all time. Global media covered the historic event, with Forbes, Business Insider, Coindesk, and many other outlets heralding in the new age of NFTs with their coverage.",
@@ -124,7 +121,6 @@ LaunchpadCard.args = {
       src="https://placekeanu.com/128/128"
       alt="keanu face"
       layout="fill"
-      objectFit="cover"
     />
   ),
   body: "7,777 Lucky Duckies have left the pond and are getting into mischief!",
