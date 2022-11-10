b96df195ea06b058bcc9aa86b564dcb4faa6e902
Nick Hong <me@nickhong.xyz>
2022-11-10 10:42:19 -0500

    Remove purge config used in tailwind 2 but not 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       animation: {
