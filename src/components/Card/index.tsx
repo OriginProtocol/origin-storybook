36f96b87fb283c4bb8ddafea9bf06fa2cf99fdc9
rolandpo <rolopod@gmail.com>
2022-12-05 21:55:37 +0100

    move title to top

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -50,7 +50,7 @@ export const Card = ({
     <a href={linkHref} download={webProperty === 'press'} target={target} className={`rounded-xl w-full flex ${webProperty === 'ousd' ? 'group bg-gradient-to-r from-[#8c66fc] to-[#0274f1]' : ''}`}>
       <div className={`flex ${webProperty === 'ousd' ? 'w-full h-full rounded-xl transition duration-300 bg-[#00000080] group-hover:bg-[#00000000] shadow-black group-hover:drop-shadow-2xl' : ''}`}>
         <div className={`m-[1px] rounded-xl w-full shadow overflow-hidden flex flex-col ${webProperty === 'ousd' ? 'bg-[#1e1f25] text-[#fafbfb]' : 'bg-white text-black'}`}>
-          <div className="w-full h-full relative overflow-hidden">
+          <div className="w-full relative overflow-hidden">
             {img}
           </div>
           <div className="px-6 py-8 w-full flex grow relative">
